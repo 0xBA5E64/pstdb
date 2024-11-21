694288b144b0da39b91bce555f3bfbd908ce86b8
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2024-11-21 19:40:10 +0100

    feat: Basic Episode & Participant relational models

--- podcast/models.py
+++ podcast/models.py
@@ -4,12 +4,18 @@ from django.db import models
 
 class Participant(models.Model):
     username = models.CharField(max_length=40)
-    realname = models.CharField(max_length=40)
-    description = models.CharField(max_length=240)
+    realname = models.CharField(max_length=40, blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.username
 
 class Episode(models.Model):
     title = models.CharField(max_length=80)
-    description = models.ManyToManyRel
-    pub_date = models.DateTimeField()
+    description = models.TextField()
+    pub_date = models.DateField()
     youtube_id = models.CharField(max_length=11)
     participants = models.ManyToManyField(Participant)
+
+    def __str__(self):
+        return self.title
