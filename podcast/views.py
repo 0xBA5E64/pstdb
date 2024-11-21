bb7c0975d29cd53411ffd7988342af828073295b
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2024-11-21 20:03:35 +0100

    feat: Rudimentary episode index page

--- podcast/views.py
+++ podcast/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Episode
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("PSTDB Index page.")
+#def index(request):
+#    return HttpResponse("PSTDB Index page.")
+
+def episode_index(request):
+    episodes = Episode.objects.all()
+    context = {
+        "episodes": episodes
+    }
+    template = loader.get_template("podcast/episode_index.html")
+    return HttpResponse(template.render(context, request))
